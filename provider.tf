cf7829f4aee98ee0ec026fc2d3bff82066347aff
prudh <prudhhviraj2@gmail.com>
2025-08-02 14:29:41 +0530

    VPC and its modules has been added

--- provider.tf
+++ provider.tf
@@ -2,14 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.0.0-beta3"
+      version = "6.4.0"
     }
   }
-   backend "s3"{
-      bucket = "vpc-module-test1"
-      key    = "vpc-test" # you should have unique key, same cannot use in other repos
+  
+  backend "s3"{
+      bucket = "vpc-module-test1-july25"
+      key    = "vpc-test-25jul25" # you should have unique key, same cannot use in other repos
       region = "us-east-1"
-      dynamodb_table = "vpc-module-test1"
+      dynamodb_table = "vpc-module-test1-july25"
   }
 }
 
